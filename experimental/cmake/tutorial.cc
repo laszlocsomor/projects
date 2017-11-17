845bdfa8dd6605bb0b087d797afb75eaca7d1dc5
Laszlo Csomor <laszlocsomor@google.com>
2017-11-17 22:59:33 +0100

    CMake: add MakeVar-configurable header file

--- experimental/cmake/tutorial.cc
+++ experimental/cmake/tutorial.cc
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "generated.h"
 
 static const int FOO = 42;
 
 int main(int argc, char** argv) {
   std::cout << "hello cmake" << FOO << std::endl;
+  std::cout << MyMakeVar << std::endl;
   return 0;
 }
