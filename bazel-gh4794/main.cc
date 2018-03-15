c802b5953c4ca9046d1023ba59b4e6bcfb384ce2
Laszlo Csomor <laszlocsomor@google.com>
2018-03-15 10:21:44 +0100

    Example project for Bazel issue #4794
    See https://github.com/bazelbuild/bazel/issues/4794

---
+++ bazel-gh4794/main.cc
@@ -0,0 +1,7 @@
+#include "my-lib.h"
+#include <iostream>
+
+int main() {
+  std::cout << "hello, foo(5)=" << foo(5) << ", foo(-3)=" << foo(-3) << std::endl;
+  return 0;
+}
---
+++ bazel-gh4794/my-lib-src.cc
@@ -0,0 +1,9 @@
+int foo(int arg) {
+  if (arg > 0) {
+    return 1;
+  } else if (arg == 0) {
+    return 0;
+  } else {
+    return -1;
+  }
+}
