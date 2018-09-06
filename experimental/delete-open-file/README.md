55cd5f7058a57f4aaf08e1521502bace2865effa
Laszlo Csomor <laszlocsomor@google.com>
2018-09-06 10:12:29 +0200

    Demo: reading from deleted file

---
+++ experimental/delete-open-file/README.md
@@ -0,0 +1,10 @@
+Demo what happens when deleting an open file (with deletion sharing).
+
+Output:
+```
+successfully deleted "foo.txt"
+attributes: 0xffffffff
+attributes(find): 0x00000020
+ReadFile: read 4 bytes from deleted file [c, i, c, a]
+press any key to continue . . .
+```
---
+++ experimental/delete-open-file/main.cc
@@ -0,0 +1,49 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    printf("usage: $0 <path-to-delete>\n");
+    return 1;
+  }
+
+  HANDLE h = CreateFileA(
+      argv[1], GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+      NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+  if (h == INVALID_HANDLE_VALUE) {
+    printf("cannot open \"%s\"\n", argv[1]);
+    return 1;
+  }
+  if (!DeleteFileA(argv[1])) {
+    printf("cannot delete \"%s\"\n", argv[1]);
+    return 1;
+  }
+  printf("successfully deleted \"%s\"\n", argv[1]);
+
+  // Get attributes via GetFileAttributesA: returns INVALID_FILE_ATTRIBUTES.
+  DWORD attr = GetFileAttributesA(argv[1]);
+  printf("attributes: 0x%08x\n", attr);
+
+  // Get attributes via FindFirstFile: can retrieve the attributes.
+  WIN32_FIND_DATAA find;
+  HANDLE h2 = FindFirstFileA(argv[1], &find);
+  if (h2 != INVALID_HANDLE_VALUE) {
+    printf("attributes(find): 0x%08x\n", find.dwFileAttributes);
+    FindClose(h2);
+  }
+
+  // Read from the file.
+  DWORD data, read;
+  BOOL succ = ReadFile(h, &data, 4, &read, NULL);
+  if (succ != FALSE) {
+    printf("ReadFile: read %d bytes from deleted file [%c, %c, %c, %c]\n",
+        read, data & 0xff, (data >> 8) & 0xff, (data >> 16) & 0xff, (data >> 24) & 0xff);
+  } else {
+    printf("ReadFile: could not read from deleted file\n");
+  }
+  
+  printf("press any key to continue . . .\n");
+  ReadFile(GetStdHandle(STD_INPUT_HANDLE), &data, 1, &read, NULL);
+  CloseHandle(h);
+  return 0;
+}
