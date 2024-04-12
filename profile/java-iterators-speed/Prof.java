b0f4286c901f029d2498aa4dae47bccaf8d64f1c
László Csomor <laszlo@engflow.com>
2024-04-12 21:41:49 +0200

    for/iterator/stream profiler: time with Instant

--- profile/java-iterators-speed/Prof.java
+++ profile/java-iterators-speed/Prof.java
@@ -1,62 +1,73 @@
-public class Prof {
-  private static final int ARRAY_SIZE = 50000000;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class Prof {
+  private static final int ARRAY_SIZE = 50_000_000;
   private static final int RUNS = 10;
 
   public static void main(String[] args) {
     String[] a = new String[ARRAY_SIZE];
     System.out.println("Filling the array...");
-    long start = System.currentTimeMillis();
+    Instant start = Instant.now();
     for (int i = 0; i < a.length; i++) {
       a[i] = new String(); // Integer.toString(i);
     }
-    long end = System.currentTimeMillis();
-    System.out.printf("Filling the array took %d ms%n", end - start);
+    System.out.printf("Filling the array took %s%n", Duration.between(start, Instant.now()));
 
-    long[] results_loop = new long[RUNS];
-    long[] results_iter = new long[RUNS];
-    long[] results_stream = new long[RUNS];
+    Duration[] results_loop = new Duration[RUNS];
+    Duration[] results_iter = new Duration[RUNS];
+    Duration[] results_stream = new Duration[RUNS];
 
     for (int i = 0; i < RUNS; i++) {
       long dummy = 0;
       System.out.printf("for-loop, run #%d%n", i);
-      start = System.currentTimeMillis();
+      start = Instant.now();
       for (int j = 0; j < a.length; j++) {
         dummy += a[j].length();
       }
-      end = System.currentTimeMillis();
-      results_loop[i] = end - start;
+      results_loop[i] = Duration.between(start, Instant.now());
 
       dummy = 0;
       System.out.printf("iterator, run #%d%n", i);
-      start = System.currentTimeMillis();
+      start = Instant.now();
       for (String s : a) {
         dummy += s.length();
       }
-      end = System.currentTimeMillis();
-      results_iter[i] = end - start;
+      results_iter[i] = Duration.between(start, Instant.now());
 
       dummy = 0;
       int dummy_arr[] = new int[] {0};
       System.out.printf("stream, run #%d%n", i);
-      start = System.currentTimeMillis();
+      start = Instant.now();
       java.util.Arrays.stream(a).forEach(s -> dummy_arr[0] += s.length());
-      end = System.currentTimeMillis();
-      results_stream[i] = end - start;
+      results_stream[i] = Duration.between(start, Instant.now());
     }
 
-    System.out.println("For-loop:");
-    for (int i = 0; i < results_loop.length; i++) {
-      System.out.printf("  %d ms%n", results_loop[i]);
-    }
+    System.out.println("\t\t| Min\t\t| Max\t\t| Median");
+    System.out.printf(
+        "For-loop:\t| %s\t| %s\t| %s%n",
+        findMin(results_loop), findMax(results_loop), findMedian(results_loop));
+    System.out.printf(
+        "Iterator:\t| %s\t| %s\t| %s%n",
+        findMin(results_iter), findMax(results_iter), findMedian(results_iter));
+    System.out.printf(
+        "Stream-ForEach:\t| %s\t| %s\t| %s%n",
+        findMin(results_stream), findMax(results_stream), findMedian(results_stream));
+  }
 
-    System.out.println("Iterator:");
-    for (int i = 0; i < results_iter.length; i++) {
-      System.out.printf("  %d ms%n", results_iter[i]);
-    }
+  private static Duration findMin(Duration... samples) {
+    return Arrays.stream(samples).min(Duration::compareTo).get();
+  }
 
-    System.out.println("Stream forEach:");
-    for (int i = 0; i < results_stream.length; i++) {
-      System.out.printf("  %d ms%n", results_stream[i]);
-    }
+  private static Duration findMax(Duration... samples) {
+    return Arrays.stream(samples).max(Duration::compareTo).get();
+  }
+
+  private static Duration findMedian(Duration... samples) {
+    List<Duration> result = Arrays.stream(samples).sorted().collect(Collectors.toList());
+    return result.get(result.size() / 2);
   }
 }
